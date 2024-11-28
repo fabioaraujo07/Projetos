33580b10e9ea037a6fe5235d32112bea0e4cbc40
Fabio Araújo <fabiogeovane925@gmail.com>
2024-11-28 23:46:38 +0000

    init commit

--- ED_TP/ED_TP/src/main/java/Classes/Mission.java
+++ ED_TP/ED_TP/src/main/java/Classes/Mission.java
@@ -8,4 +8,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import Collections.Lists.LinkedUnorderedList;
 
 public class Mission {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
